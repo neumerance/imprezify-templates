863bfa525893bbf4181bd1166f51681c06c776c6
jonathan canaveral <neumerance@live.com>
2019-09-21 00:15:14 +0800

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="app">
+    <v-app>
+      <v-content>
+        <v-container>Hello world</v-container>
+      </v-content>
+    </v-app>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+  components: {
+  }
+}
+</script>
